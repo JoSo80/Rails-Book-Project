e8754084bf8729e7e9162c518c3d9ba39a85827a
JoSo80 <josrobsor@gmail.com>
2020-10-11 17:38:38 -0500

    Added 100.Books.csv and tested with seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+Book.delete_all
+Publisher.delete_all
+
+filename = Rails.root.join("db/100.Books.csv")
+
+puts "Loading Books from the CSV file: #{filename}"
+
+csv_data = File.read(filename)
+books = CSV.parse(csv_data, headers: true, encoding: "iso-8859-1")
+
+books.each do |b|
+  puts b["title"]
+end
