3abe7ce8f26a0b4d6cbee526c32da23bf67b582d
JoSo80 <josrobsor@gmail.com>
2020-10-11 18:28:27 -0500

    Updated seed script to add books and publishers from .csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,25 @@ filename = Rails.root.join("db/100.Books.csv")
 puts "Loading Books from the CSV file: #{filename}"
 
 csv_data = File.read(filename)
-books = CSV.parse(csv_data, headers: true, encoding: "iso-8859-1")
+bookz = CSV.parse(csv_data, headers: true, encoding: "iso-8859-1")
 
-books.each do |b|
-  puts b["title"]
+bookz.each do |b|
+  publisher = Publisher.find_or_create_by(name: b["publisher"])
+
+  if publisher&.valid?
+    book = publisher.books.create(
+      title:            b["title"],
+      author:           b["authors"],
+      publication_year: b["publication_year"],
+      number_of_pages:  b["num_pages"],
+      average_rating:   b["average_rating"]
+    )
+
+    puts "invalid book #{b['title']}" unless book&.valid?
+  else
+    puts "invalid publisher #{b['publisher']} for book #{b['title']}."
+  end
 end
+
+puts "Created #{Publisher.count} Publishers"
+puts "Created #{Book.count} Books"
