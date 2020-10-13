54b6c9c2f3bde82215cb21038471449e3d297afa
JoSo80 <josrobsor@gmail.com>
2020-10-12 21:25:45 -0500

    Updated home controller and home view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
                  .order("average_rating DESC")
                  .limit(10)
 
-    @publishers = Publisher.limit(10)
+    @publishers = Publisher.order_by_books
+                           .limit(10)
   end
 end
