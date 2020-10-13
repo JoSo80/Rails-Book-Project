9ee824893a126041e0b8376f21f9d5c87b74fc7e
JoSo80 <josrobsor@gmail.com>
2020-10-12 20:37:56 -0500

    Updated home view to show top 10 books

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @books = Book.includes(:publisher)
+                 .order("average_rating DESC")
+                 .limit(10)
+
+    @publishers = Publisher.limit(10)
   end
 end
