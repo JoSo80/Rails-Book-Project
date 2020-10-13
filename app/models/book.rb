ec7f3f7fa5a30704f0f7ba0f67a78196743bccee
JoSo80 <josrobsor@gmail.com>
2020-10-12 19:44:07 -0500

    Updated books and publishers index & show views

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :publisher
 
-  validates :title, :author, :publication_year, :average_rating, presence: true
+  validates :title, :author, :publication_year, :number_of_pages, :average_rating, presence: true
   validates :title, uniqueness: true
-  validates :publication_year, :average_rating, numericality: true
+  validates :publication_year, :number_of_pages, :average_rating, numericality: true
 end
